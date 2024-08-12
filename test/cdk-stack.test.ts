826e4ea849cad7d61e32763fb42cfcae76b038be
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-08-12 22:44:00 +0200

    Add SNS to SQS test

--- test/cdk-stack.test.ts
+++ test/cdk-stack.test.ts
@@ -1,4 +1,5 @@
 import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { CdkStack } from '../src/cdk-stack';
@@ -9,6 +10,13 @@ const sqsClient = new SQSClient({
 });
 
 const QUEUE_URL = 'http://localhost:4566/000000000000/CreateServerlessAwsWebhookQueue';
+
+const snsClient = new SNSClient({
+  region: 'us-east-1',
+  endpoint: 'http://127.0.0.1:4566',
+});
+
+const TOPIC_ARN = 'arn:aws:sns:us-east-1:000000000000:CreateServerlessAwsWebhookTopic';
 
 describe('CreateServerlessAwsWebhook unit', () => {
   test('SQS Queue and SNS Topic Created', () => {
@@ -53,6 +61,44 @@ describe('CreateServerlessAwsWebhook unit', () => {
     expect(Messages).not.toBeUndefined();
     expect(Messages?.length).toBeGreaterThan(0);
     const receivedMessage = JSON.parse((Messages as any)[0].Body);
+    expect(receivedMessage).toEqual(messageBody);
+
+    // Clean up by deleting the message from the queue
+    const deleteParams = {
+      QueueUrl: QUEUE_URL,
+      ReceiptHandle: (Messages as any)[0].ReceiptHandle,
+    };
+
+    await sqsClient.send(new DeleteMessageCommand(deleteParams));
+  });
+
+  test('should send an event to SNS and verify it is in the SQS queue', async () => {
+    // Define the message to send
+    const messageBody = {
+      message: 'fakeMessage',
+    };
+
+    // Send the message
+    const publishParams = {
+      TopicArn: TOPIC_ARN,
+      Message: JSON.stringify(messageBody),
+    };
+
+    await snsClient.send(new PublishCommand(publishParams));
+
+    // Receive the message
+    const receiveParams = {
+      QueueUrl: QUEUE_URL,
+      MaxNumberOfMessages: 1,
+      WaitTimeSeconds: 5, // Adjust as necessary
+    };
+
+    const { Messages } = await sqsClient.send(new ReceiveMessageCommand(receiveParams));
+
+    // Verify the message is in the queue
+    expect(Messages).not.toBeUndefined();
+    expect(Messages?.length).toBeGreaterThan(0);
+    const receivedMessage = JSON.parse(JSON.parse((Messages as any)[0].Body).Message);
     expect(receivedMessage).toEqual(messageBody);
 
     // Clean up by deleting the message from the queue
